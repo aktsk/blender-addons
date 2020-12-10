7d807754ac220881bea42f2fadbb07a91086672a
takanakahiko <takanakahiko@gmail.com>
2020-12-10 19:26:33 +0900

    add process for change angle and refactor

--- scripts/addons/undo_without_camera/__init__.py
+++ scripts/addons/undo_without_camera/__init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "version": (1, 0),
     "blender": (2, 91, 0),
     "location": "",
-    "description": "redo時も反転状態を維持するやつ",
+    "description": "redo時も反転や回転の状態を維持します",
     "warning": "",
     "support": "TESTING",
     "wiki_url": "",
@@ -14,28 +14,42 @@ bl_info = {
 
 import bpy
 
-scale_x = 1
 
+@bpy.app.handlers.persistent
 def handler_undo_pre(scene):
-    print('pre')
-    global scale_x
-    scale_x = bpy.data.objects['Camera'].scale.x
+    print('undo_pre')
 
+    vars(handler_undo_pre)["scale.x"] = scene.camera.scale.x
+    vars(handler_undo_pre)["rotation_euler.y"] = scene.camera.rotation_euler.y
+
+
+@bpy.app.handlers.persistent
 def handler_undo_post(scene):
-    print('post')
-    if bpy.data.objects['Camera'].scale.x != scale_x :
-        bpy.data.objects['Camera'].scale.x = scale_x
-        bpy.ops.ed.undo()
+    print('undo_post')
+
+    scale_x = vars(handler_undo_pre)["scale.x"]
+    if scene.camera.scale.x != scale_x :
+        scene.camera.scale.x = scale_x
+        bpy.ops.ed.undo() # 反転方法によって、この処理が必要でない可能性がある
+
+    rotation_euler_y = vars(handler_undo_pre)["rotation_euler.y"]
+    if scene.camera.rotation_euler.y != rotation_euler_y :
+        scene.camera.rotation_euler.y = rotation_euler_y
+        bpy.ops.ed.undo() # 反転方法によって、この処理が必要でない可能性がある
 
 def register():
     print("register")
     bpy.app.handlers.undo_pre.append(handler_undo_pre)
     bpy.app.handlers.undo_post.append(handler_undo_post)
+    bpy.app.handlers.redo_pre.append(handler_undo_pre)
+    bpy.app.handlers.redo_post.append(handler_undo_post)
 
 def unregister():
     print("unregister")
     bpy.app.handlers.undo_pre.remove(handler_undo_pre)
     bpy.app.handlers.undo_post.remove(handler_undo_post)
+    bpy.app.handlers.redo_pre.remove(handler_undo_pre)
+    bpy.app.handlers.redo_post.remove(handler_undo_post)
 
 if __name__ == "__main__":
     register()
